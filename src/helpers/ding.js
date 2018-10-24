f093c7019615d0d3f252257bf80c1848eacf504f
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-10-24 17:05:14 +0800

    Add chirps

--- src/helpers/ding.js
+++ src/helpers/ding.js
@@ -2,6 +2,7 @@
 
 import Notifier from 'node-notifier'
 import Play from 'play-sound'
+import { resolve } from 'path'
 
 const Player = Play({})
 
@@ -9,14 +10,13 @@ export type Props = {
   count: number
 }
 
+export const SOUNDS_PATH = resolve(__dirname, '..', '..', 'audio')
+
 /** Path to the sound file to be played */
-export const SOUND_FILE = require('path').resolve(
-  __dirname,
-  '..',
-  '..',
-  'audio',
-  'oringz-w447.ogg'
-)
+export const LONG_SOUND_FILE = resolve(SOUNDS_PATH, 'oringz-w447.ogg')
+
+/** Path to the sound file to be played */
+export const CHIRP_SOUND_FILE = resolve(SOUNDS_PATH, 'digital-spit.ogg')
 
 /**
  * Sends notifications
@@ -32,5 +32,19 @@ export const ding = (opts: Props) => {
     message: 'Ding!'
   })
 
-  Player.play(SOUND_FILE)
+  Player.play(LONG_SOUND_FILE)
+  setTimeout(() => {
+    chirp(count)
+  }, 1000)
+}
+
+/** Plays the short sound file */
+function chirp(count = 1) {
+  Player.play(CHIRP_SOUND_FILE)
+
+  if (count > 1) {
+    setTimeout(() => {
+      chirp(Math.min(count - 1, 3))
+    }, 150)
+  }
 }
