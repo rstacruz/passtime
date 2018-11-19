f2a249094420819d6cccb618e8e8f51032619d55
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-11-19 13:23:29 +0800

    Support writing minutes without m

--- src/helpers/__tests__/string_to_ms.test.js
+++ src/helpers/__tests__/string_to_ms.test.js
@@ -1,6 +1,13 @@
+/* eslint-env jest */
+
 import stringToMs from '../string_to_ms'
 
-const ASSERTIONS = [['1m', 60000]]
+const ASSERTIONS = [
+  ['1m', 60000],
+  ['10m', 600000],
+  ['20s', 20000],
+  ['10', 600000]
+]
 
 ASSERTIONS.forEach(([input, output]) => {
   it(`${input} -> ${output}`, () => {
--- src/helpers/string_to_ms.js
+++ src/helpers/string_to_ms.js
@@ -1,6 +1,12 @@
 import stringToMs from 'ms'
 
-const generousStringToMs = (str /*: string */) => {
+const generousStringToMs = (str: string): number => {
+  const minutes = +str
+
+  if (!isNaN(minutes)) {
+    return minutes * 60000
+  }
+
   return stringToMs(str)
 }
 
